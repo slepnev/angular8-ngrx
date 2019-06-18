ac82d439c93ffb467e51a6974fc2e0b319adb162
Слепнев Александр Юрьевич <ayslepnev@domclick.ru>
2019-06-18 18:38:53 +0300

    auth: ng8

--- src/app/courses/course.reducer.ts
+++ src/app/courses/course.reducer.ts
@@ -1,13 +1,42 @@
-import { Action } from '@ngrx/store';
 import { Course } from './model/course';
-import { Lesson } from './model/lesson';
 import { createEntityAdapter, EntityAdapter, EntityState } from '@ngrx/entity';
+import { CourseActions, CourseActionTypes } from './course.actions';
 
 
 export interface CoursesState extends EntityState<Course> {
-
-
+  allCoursesLoaded: boolean;
 }
 
 export const adapter: EntityAdapter<Course> = createEntityAdapter<Course>();
+
+export const initalCoursesState: CoursesState = adapter.getInitialState({
+  allCoursesLoaded: false
+});
+
+export function coursesReducer(state = initalCoursesState, action: CourseActions): CoursesState {
+  switch (action.type) {
+
+    case CourseActionTypes.CourseLoaded:
+      return adapter.addOne(action.payload.course, state);
+
+    case CourseActionTypes.AllCoursesLoaded:
+      return adapter.addAll(action.payload.courses, {...state, allCoursesLoaded: true});
+
+    case CourseActionTypes.CourseSaved:
+      return adapter.updateOne(action.payload.course, state);
+
+    default: {
+
+      return state;
+
+    }
+  }
+}
+
+export const {
+  selectAll,
+  selectEntities,
+  selectIds,
+  selectTotal
+} = adapter.getSelectors();
 
--- src/app/courses/services/course.resolver.ts
+++ src/app/courses/services/course.resolver.ts
@@ -1,24 +1,36 @@
-
-
-
-import {Injectable} from "@angular/core";
-import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from "@angular/router";
-import {Course} from "../model/course";
-import {Observable} from "rxjs";
-import {CoursesService} from "./courses.service";
-
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
+import { Course } from '../model/course';
+import { Observable } from 'rxjs';
+import { CoursesService } from './courses.service';
+import { select, Store } from '@ngrx/store';
+import { AppState } from '../../reducers';
+import { selectCourseById } from '../course.selectors';
+import { filter, first, tap } from 'rxjs/operators';
+import { CourseRequested } from '../course.actions';
 
 
 @Injectable()
 export class CourseResolver implements Resolve<Course> {
 
-    constructor(private coursesService:CoursesService) {
+  constructor(private coursesService: CoursesService, private store: Store<AppState>) {
 
-    }
+  }
 
-    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Course> {
-        return this.coursesService.findCourseById(route.params['id']);
-    }
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Course> {
+    const courseId = route.params['id'];
+
+    return this.store.pipe(
+      select(selectCourseById(courseId)),
+      tap(course => {
+        if (!course) {
+          this.store.dispatch(new CourseRequested({courseId}));
+        }
+      }),
+      filter(course => !!course),
+      first()
+    );
+  }
 
 }
 
